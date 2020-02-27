ccd7bf9a575bf7da2f6329dcad9225f4dc0b93f6
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-02-27 01:30:04 -0800

    Creatures can now step along paths
    Also fixed Creature.step_rotate to not overshoot target angle

--- hungry_and_slow/Creature.gd
+++ hungry_and_slow/Creature.gd
@@ -9,12 +9,12 @@ var path = PoolVector2Array() setget set_path
 
 
 func _ready():
-	update_direction(self.direction_vec)
+	update_direction(direction_vec)
 
 
 func update_direction(dir : Vector2) -> void :
-	self.direction_vec = dir.normalized()
-	self.rotation = atan2(dir.y,dir.x)+PI/2
+	direction_vec = dir.normalized()
+	rotation = atan2(dir.y,dir.x)+PI/2
 
 
 func step_rotate(target_direction : Vector2, delta_t : float) -> void:
@@ -22,21 +22,34 @@ func step_rotate(target_direction : Vector2, delta_t : float) -> void:
 	if abs(angle_difference) < 0.02:
 		update_direction(target_direction)
 	else:
-		update_direction( direction_vec.rotated((max_rot_speed * delta_t) * angle_difference/PI) )
+		var delta_theta = (max_rot_speed * delta_t) * angle_difference/PI
+		if abs(delta_theta) > abs(angle_difference):
+			delta_theta = angle_difference
+		update_direction( direction_vec.rotated(delta_theta) )
 
 
 
 
 func step_move_ahead(dist : float) -> void:
-	self.position += dist * direction_vec
+	position += dist * direction_vec
 
 
 
 func set_path(new_path):
 	path = new_path
 
-func step_path(dist : float) -> void:
-	pass
+func step_path(dist : float, delta_t : float) -> void:
+	if not path or dist <= 0 : return
+	step_rotate(path[0]-position,delta_t)
+	var segment_length = position.distance_to(path[0])
+	if dist < segment_length :
+		position += dist * ((path[0]-position) / segment_length)
+	elif dist >= segment_length :
+		position = path[0]
+		path.remove(0)
+		dist -= segment_length
+		step_path(dist, delta_t)
+		
 
 
 
--- hungry_and_slow/Player.gd
+++ hungry_and_slow/Player.gd
@@ -11,14 +11,8 @@ export var speed_control_radius = 400
 
 
 func _ready():
-	create_mask()
-	
-	# Test stuff here
-	var test_timer = get_tree().create_timer(1.0)
-	yield(test_timer,"timeout")
-	path.push_back(Vector2(0,128))
-	path.push_back(Vector2(64,128))
-	path.push_back(Vector2(64,64))
+#	create_mask()
+	pass
 
 
 
@@ -35,9 +29,6 @@ func _process(delta):
 		
 		var speed = max_speed * min(rel_mouse_pos.length(),speed_control_radius)/float(speed_control_radius)
 		step_move_ahead(speed * delta)
-	
-	#Test stuff here
-	step_path(max_speed/3 * delta)
 
 
 
