9af7fd9b87bd1dfdf49e9493210a5d805ecbdeaa
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-03-08 22:09:01 -0700

    add camera method that returns global Rect2 representing what's visible

--- hungry_and_slow/TightCamera.gd
+++ hungry_and_slow/TightCamera.gd
@@ -2,3 +2,9 @@ extends Camera2D
 
 func _process(delta):
 	align()
+
+# Gets global rect of camera's view
+func get_rect() -> Rect2:
+	var size : Vector2 = get_viewport().size
+	return Rect2(to_global(position)-size/2.0,size)
+	
